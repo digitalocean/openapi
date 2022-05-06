75d8969c475cbab95a14b21835e3beb21e1a361f
Samuel Guyah <septimusguy@gmail.com>
2022-05-06 10:39:38 -0500

    With camel (#630)
    * AutoRest++ (#601)
    
    * autorest-basics
    
    * add typescript
    
    * getting past the allOf error. lint is now broken
    
    * remove format:string
    
    * fixed lint error
    
    * no example when example?
    
    * example
    
    * lenient
    
    * commented endpoints with redundanies
    
    * generated code
    
    * removed
    
    * creds
    
    * first directive
    
    * update py version
    
    * correct yaml
    
    * 1click
    
    * removed generated
    
    * enpoints back
    
    * delete auto
    
    * two left
    
    * add back
    
    * fix?
    
    * what

--- spectral/functions/ensureSnakeCaseWithDigits.js
+++ spectral/functions/ensureSnakeCaseWithDigits.js
@@ -1,17 +1,17 @@
 /*
  *
- * Ensures the the property is in snake_case (this may include camelCase) and allows for digits.
+ * Ensures the the property is in snake_case and allows for digits.
  * A custom function is needed as the built-in Spectral rule does not
  * allow for leading digits.
  *
  */
 
 module.exports = (param, _, paths) => {
-    const re = RegExp('^(([a-z]+([A-Z0-9a-z]+)*([A-Z])*)_)+[a-zA-Z0-9]+$');
+    const re = RegExp('^(([A-Z0-9a-z]+([A-Z])*)(\.|_)*)+[A-Z0-9a-z]+$');
 
     if (re.test(param)) { return }
 
     return [{
-        message: `${paths.target ? paths.target.join('.') : 'property'} is not snake_case or camelCase`
+        message: `${paths.target ? paths.target.join('.') : 'property'} is not snake_case`
     }]
 }
