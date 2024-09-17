21e0351cfb308fca525c614ca854ae9921545ad7
vsharma6855 <vsharma@digitalocean.com>
2024-09-17 20:07:13 +0530

    DBaaS | Add API endpoint for applying cluster patches (#909)
    * [DBAAS-35] | Add API endpoint for applying cluster patches
    
    * Update specification/resources/databases/databases_update_installUpdate.yml
    
    Co-authored-by: Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
    
    * Update specification/resources/databases/databases_update_installUpdate.yml
    
    Co-authored-by: danaelhe <42972711+danaelhe@users.noreply.github.com>
    
    * changed the structure
    
    * Add 'install' to linter rules for allowed operation names.
    
    ---------
    
    Co-authored-by: v.sharma <v.sharma@digitalocean.com>
    Co-authored-by: Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
    Co-authored-by: danaelhe <42972711+danaelhe@users.noreply.github.com>
    Co-authored-by: Andrew Starr-Bochicchio <a.starr.b@gmail.com>

--- spectral/functions/validateOpIDNaming.js
+++ spectral/functions/validateOpIDNaming.js
@@ -11,7 +11,7 @@ const PATCH = ["patch"];
 const POST = ["create", "post", "add", "tag", "install", "reset", "upgrade",
   "recycle", "run", "retry", "validate", "assign", "unassign", "cancel", "list",
   "destroy", "delete", "update", "attach", "revert", "commit"];
-const PUT = ["update", "promote"];
+const PUT = ["update", "promote", "install"];
 
 const articles = ["_a_", "_an_", "_the_"]
 
